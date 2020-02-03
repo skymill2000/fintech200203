d9f4a2399de135cf6820155dc8b86df775580075
skymill2000 <skymill2000@gmail.com>
2020-02-03 17:14:25 +0900

    예제 파일 완성

---
+++ dataType.js
@@ -0,0 +1,13 @@
+var length = 16; // Number
+let lastName = "Johnson"; // String
+let object = { firstName: "John", lastName: "Doe" }; // Object
+var x = 16 + "Volvo";
+console.log(object);
+var y = "16" + "Volvo";
+console.log(y);
+var z = 16 + "Volvo";
+console.log(z);
+var a = "Volvo" + 16;
+console.log(a);
+var b = 16 + 4 + "Volvo";
+console.log(b);
---
+++ functionTest.js
@@ -0,0 +1,23 @@
+function plus(a, b){
+    return a + b;
+}
+
+function minus(a, b){
+    return a - b;
+}
+
+function mul(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+
+// * / -
+
+console.log(plus(5,4));
+console.log(minus(5,4));
+console.log(mul(5,4));
+console.log(div(5,4));
