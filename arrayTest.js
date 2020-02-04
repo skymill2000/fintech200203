79767316f410714a416484ae2eaee4fc4e1322bb
skymill2000 <skymill2000@gmail.com>
2020-02-04 10:54:25 +0900

    비동기 파일 읽어오기 실행

--- arrayTest.js
+++ arrayTest.js
@@ -41,4 +41,7 @@ for(var i = 0; i < cars.length; i++){
     var element = cars[i];
     //Todo BMW라는 차가 있을때 ! 출력
     console.log(element.name);
+    if(element.name == "BMW"){
+        console.log("!");
+    }
 }
