3c54f5441c657be46fc3dc603516d6fcf65c032a
skymill2000 <skymill2000@gmail.com>
2020-02-04 15:25:59 +0900

    ajax 통신 기본 예제

--- expressHttpServer.js
+++ expressHttpServer.js
@@ -4,6 +4,10 @@ const app = express()
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
 app.get('/', function (req, res) {
   res.render('test')
 })
@@ -16,5 +20,8 @@ app.get('/member', function (req, res) {
     res.send('member page')
 })
   
+app.post('/join', function(req, res){
+  console.log(req.body)
+})
 
 app.listen(3000)
