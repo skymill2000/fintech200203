ce6b50aec1a4e8b1c3abd47eaba329cb49a8208d
skymill2000 <skymill2000@gmail.com>
2020-02-05 11:38:43 +0900

    토큰 요청 완료

--- expressHttpServer.js
+++ expressHttpServer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
- 
+var request = require('request')
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -12,5 +12,26 @@ app.get('/signup', function(req, res){
   res.render('signup');
 })
 
+app.get('/authResult', function(req, res){
+  var authCode = req.query.code;
+  console.log(authCode);
+  var option = {
+    method : "POST",
+    url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+    headers : {
+      'Content-Type' : "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    form : {
+        code : authCode,
+        client_id : 'q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF',
+        client_secret : 'yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy',
+        redirect_uri : 'http://localhost:3000/authResult',
+        grant_type : 'authorization_code'
+    }
+  }
+  request(option, function (error, response, body) {
+    res.send(body);
+  });
+})
 
 app.listen(3000)
