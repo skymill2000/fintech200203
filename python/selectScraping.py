1ef3fd990b9ca131b129e9709f6c28680501b51e
skymill2000 <skymill2000@gmail.com>
2020-02-07 15:11:42 +0900

    스크래핑 과제

---
+++ python/selectScraping.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+driver = webdriver.Chrome('./chromedriver')
+driver.implicitly_wait(1)
+driver.get('http://luris.molit.go.kr/web/index.jsp');
+driver.implicitly_wait(1)
+
+element = Select(driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[1]/ul/li[1]/select'))
+element2 = Select(driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[1]/ul/li[2]/select'))
+element3 = Select(driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[1]/ul/li[3]/select'))
+element4 = Select(driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[1]/ul/li[4]/select'))
+element5 = driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[2]/ul/li[2]/input')
+element6 = driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[2]/ul/li[4]/input')
+button = driver.find_element_by_xpath('//*[@id="gnb_tab11"]/div/div[2]/div/div[3]/button')
+
+driver.implicitly_wait(3)
+element.select_by_visible_text('전라남도')
+driver.implicitly_wait(1)
+element5.send_keys("45")
